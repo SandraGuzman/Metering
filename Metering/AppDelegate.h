a7152948f11765d660b4c506436fc0516877e4d7
SandraGuzman <sandra.guzba18@gmail.com>
2015-07-17 13:13:17 -0500

    Initial Commit

---
+++ Metering/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Metering
+//
+//  Created by Sandra Guzmán Bautista on 17/07/15.
+//  Copyright (c) 2015 onikom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
